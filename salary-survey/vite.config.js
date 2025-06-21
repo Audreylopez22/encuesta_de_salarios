71819deff53c54982bfb8e70657f5fe28a7d510b
Audrey López <audrey.lopezh@gmail.com>
2025-06-20 19:55:52 -0500

    Prueba de deploy desde nueva rama2

--- salary-survey/vite.config.js
+++ salary-survey/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/encuesta_de_salarios/', // 👈 importante para GitHub Pages
+  base: '/encuesta_de_salarios/salary-survey', // 👈 importante para GitHub Pages
   plugins: [react()],
 })
